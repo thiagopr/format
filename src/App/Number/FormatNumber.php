3b5637c52b7522c0aee26a995254123d5b290321
Thiago Oliveira <thiagopr.oliveira@gmail.com>
2016-05-28 15:11:54 -0300

    Initial functions

--- src/App/Number/FormatNumber.php
+++ src/App/Number/FormatNumber.php
@@ -5,6 +5,18 @@ namespace App\Number;
 
 class FormatNumber {
 
+    public static function padLeft($value, $lenght, $str_pad = "0") {
+        return str_pad($value, $lenght, $str_pad, STR_PAD_LEFT);
+    }
 
+    public static function money($value){
+        $value = (double)$value;
+        return number_format($value, 2, ',', '.');
+    }
+
+    function percentual($value, $length){
+        $value = (double)$value;
+        return number_format($value, $length, ',', '.');
+    }
 
 }
